b85190db6137a2b9d1286dd049a4ddf0931579b1
Heman <hemanbermudez269970@correo.itm.edu.co>
2026-04-05 22:46:40 -0500

    Se creó el UserService y el UserController. Se creó la conexión de los mismo con el DAO

--- src/main/java/com/itm/cartera_multibanco/model/User.java
+++ src/main/java/com/itm/cartera_multibanco/model/User.java
@@ -1,13 +1,26 @@
 package com.itm.cartera_multibanco.model;
 
+import jakarta.validation.constraints.NotBlank; // IMPORTANTE: Añade estos imports
+import jakarta.validation.constraints.Size;
+
+
 public class User {
 
+    @NotBlank(message = "La cédula es obligatoria")
     private String cedula;
+
+    @NotBlank(message = "El nombre no puede estar vacío")
     private String nombreCompleto;
+
+    @NotBlank(message = "El username es obligatorio")
     private String username;
+
+    @NotBlank(message = "La clave es obligatoria")
+    @Size(min = 4, message = "La clave debe tener al menos 4 caracteres")
     private String clave;
 
     public User() {
+        
     }
 
     public User(String cedula, String nombreCompleto, String username, String clave) {
@@ -48,4 +61,5 @@ public class User {
     public void setClave(String clave) {
         this.clave = clave;
     }
+
 }
